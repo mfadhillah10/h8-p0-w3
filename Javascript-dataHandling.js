3183793d2b2e78ccb5a2fd1d45179acd507ced6d
mfadhillah10 <fadhilakbarr1@gmail.com>
2018-03-07 21:43:14 +0700

    Week 3 Exercise 3

--- Javascript-dataHandling.js
+++ Javascript-dataHandling.js
@@ -1,23 +1,25 @@
-var input = [["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
-    ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
-    ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
-    ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]];
+var input = [
+  ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
+  ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
+  ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
+  ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]];
 
 var form = ['Nomor ID: ', 'Nama Lengkap: ', 'TTL: ', 'Hobi: '];
 
 function dataHandling(argumen) {
 
-  for (var i = 0; i < input.length; i++) {
+for (var i = 0; i < input.length; i++) {
+  input[i].splice(2, 2, input[i][2] + ' ' + input[i][3]);
+
+  for (var j = 0; j < input.length; j++) {
     
-    for (var j = 0; j <= input.length; j++) {
-      
-      console.log(form[j] + input[i][j]);
-    }
-    console.log('\n');
+    console.log(form[j] + input[i][j]);
   }
+  console.log('\n');
+}
 }
 dataHandling(input);
 
-/*var arr = ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"];
-arr.splice(2, 2, arr[2] + ' ' + arr[3]);
-console.log(arr);*/
+// var arr = ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"];
+/*input.splice(2, 2, input[0][2] + ' ' + input[0][3]);
+console.log(input);*/
