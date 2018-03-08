56f39b6b54a6bcd94c0a518c869424d6316acee8
mfadhillah10 <36653246+mfadhillah10@users.noreply.github.com>
2018-03-08 15:10:15 +0700

    Week 3 Exercise 14

---
+++ Javascript-kelompokAngka.js
@@ -0,0 +1,28 @@
+function mengelompokkanAngka(arr) {
+    // you can only write your code here!
+    var hasil = [[],[],[]];
+    for (var i = 0; i < arr.length; i ++) {
+        // kelipatan 3
+        if (arr[i] % 3 === 0) {
+            hasil[2].push(arr[i]);
+            continue;
+        }
+        // ganjil
+        if (arr[i] % 2 !== 0) {
+            hasil[1].push(arr[i]);
+            continue;
+        }
+        // genap
+        if (arr[i] % 2 === 0) {
+            hasil[0].push(arr[i]);
+            continue;
+        }
+    }
+    return hasil;
+  }
+  
+  // TEST CASES
+  console.log(mengelompokkanAngka([2, 4, 6])); // [ [2, 4], [], [6] ]
+  console.log(mengelompokkanAngka([1, 2, 3, 4, 5, 6, 7, 8, 9])); // [ [ 2, 4, 8 ], [ 1, 5, 7 ], [ 3, 6, 9 ] ]
+  console.log(mengelompokkanAngka([100, 151, 122, 99, 111])); // [ [ 100, 122 ], [ 151 ], [ 99, 111 ] ]
+  console.log(mengelompokkanAngka([])); // [ [], [], [] ]
